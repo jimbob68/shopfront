d9c0db89e615633889d85ea559b0ecae4eef86d6
James Fraser <jimbob4t@virginmedia.com>
2020-12-07 13:38:54 +0000

    connected to firebase.

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyBt2nY8TGQAaVC8yUT445EQsr5_xR8UHrM',
+	authDomain: 'shopfront-7810d.firebaseapp.com',
+	projectId: 'shopfront-7810d',
+	storageBucket: 'shopfront-7810d.appspot.com',
+	messagingSenderId: '789220340468',
+	appId: '1:789220340468:web:92a096b9797876a26964a1',
+	measurementId: 'G-Q0QBNS76Q9'
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+export default db;
--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Products.css';
+import firebase from 'firebase/app';
+import db from '../firebaseConfig.js';
 
 const Products = () => {
+	const [ products, setProducts ] = useState([]);
+
+	useEffect(() => {
+		retrieveProducts();
+	});
+
+	const retrieveProducts = async () => {
+		const productRef = db.collection('products').doc('HKoA7R3vTQXzvMk8DvOV');
+		const doc = await productRef.get();
+		setProducts(doc.data());
+	};
 	return <p>Products</p>;
 };
 export default Products;
