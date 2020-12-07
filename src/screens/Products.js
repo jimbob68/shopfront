0449866747b7fa4b16d0cd9f268c053b5a8ffccd
James Fraser <jimbob4t@virginmedia.com>
2020-12-07 14:54:18 +0000

    Product details coming through from firebase.

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,20 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import './Products.css';
 import firebase from 'firebase/app';
+import 'firebase/storage';
+// import * as firebase from 'firebase';
 import db from '../firebaseConfig.js';
 
 const Products = () => {
 	const [ products, setProducts ] = useState([]);
+	const [ productImage, setProductImage ] = useState('');
 
 	useEffect(() => {
 		retrieveProducts();
-	});
+	}, []);
 
 	const retrieveProducts = async () => {
 		const productRef = db.collection('products').doc('HKoA7R3vTQXzvMk8DvOV');
 		const doc = await productRef.get();
 		setProducts(doc.data());
+		const imageRef = firebase.storage().ref(doc.data().image);
+		imageRef.getDownloadURL().then((imageUrl) => {
+			setProductImage(imageUrl);
+		});
 	};
-	return <p>Products</p>;
+	return (
+		<div>
+			<h1>Products</h1>
+
+			<p>Name: {products.name}</p>
+			<img src={productImage} alt="product" />
+			<p>Price: £{products.price}</p>
+			<p>Description: {products.description}</p>
+		</div>
+	);
 };
 export default Products;
