7bc259ed10ff4077b2f13beac6a8ac1e3540aa19
James Fraser <jimbob4t@virginmedia.com>
2020-12-08 13:07:52 +0000

    problems displaying images from firebase.

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -7,29 +7,99 @@ import db from '../firebaseConfig.js';
 
 const Products = () => {
 	const [ products, setProducts ] = useState([]);
-	const [ productImage, setProductImage ] = useState('');
+	const [ productImages, setProductImages ] = useState([]);
 
 	useEffect(() => {
 		retrieveProducts();
 	}, []);
 
 	const retrieveProducts = async () => {
-		const productRef = db.collection('products').doc('HKoA7R3vTQXzvMk8DvOV');
-		const doc = await productRef.get();
-		setProducts(doc.data());
-		const imageRef = firebase.storage().ref(doc.data().image);
-		imageRef.getDownloadURL().then((imageUrl) => {
-			setProductImage(imageUrl);
+		const productsRef = db.collection('products');
+		const productsData = await productsRef.get();
+		console.log('productsData', productsData);
+		// setProducts(
+		// 	productsData.docs.map((doc) => {
+		// 		const data = doc.data();
+		// 		retrieveImage(data);
+		// 		// const imageRef = await firebase.storage().ref(doc.data().image);
+		// 		// imageRef.getDownloadURL().then((imageURL) => {
+		// 		// 	data.image = imageURL;
+		// 		// 	console.log('imageURL', imageURL);
+		// 		// });
+		// 		console.log('data', data);
+		// 		return data;
+		// 	})
+		// );
+
+		const allProductsData = productsData.docs.map((doc) => {
+			// const data = doc.data();
+			// retrieveImage(data);
+			// const imageRef = await firebase.storage().ref(doc.data().image);
+			// imageRef.getDownloadURL().then((imageURL) => {
+			// 	data.image = imageURL;
+			// 	console.log('imageURL', imageURL);
+			// });
+			// console.log('data', data);
+			return doc.data();
 		});
+		console.log('ALLproductsData', allProductsData);
+
+		allProductsData.forEach((product) => {
+			const imageRef = firebase.storage().ref(product.image);
+			imageRef.getDownloadURL().then((imageURL) => {
+				product.image = imageURL;
+				// console.log('imageURL', imageURL);
+			});
+			// .then(setProducts(products.push(product.image)));
+		});
+		console.log('ALLproductsData2', allProductsData);
+		setProducts(allProductsData);
+
+		// const imageRef = firebase.storage().ref(doc.data().image);
+		// imageRef.getDownloadURL().then((imageUrl) => {
+		// 	setProductImage(imageUrl);
+		// });
+	};
+	const retrieveImage = (data) => {
+		const imageRef = firebase.storage().ref(data.image);
+		imageRef.getDownloadURL().then((imageURL) => {
+			const temporaryProductImages = productImages.slice();
+			temporaryProductImages.push(imageURL);
+			setProductImages(temporaryProductImages);
+			console.log('imageURL', imageURL);
+		});
+		return data;
+	};
+	const displayProductsInfo = () => {
+		console.log('products!!!!!!', products[0]);
+		const productsInfo = products.map((product) => {
+			console.log('IMAGE!!!!!', product.image);
+			// let productImage;
+			// const imageRef = firebase.storage().ref(product.image);
+			// imageRef.getDownloadURL().then((imageURL) => {
+			// 	productImage = imageURL;
+			// 	console.log('imageURL', imageURL);
+			// });
+			return (
+				<div>
+					<p>Name: {product.name}</p>
+					{console.log('product', product.image)}
+					<img src={product.image} alt="product" />
+					<p>Price: £{product.price}</p>
+					<p>Description: {product.description}</p>
+				</div>
+			);
+		});
+		return productsInfo;
 	};
 	return (
 		<div>
 			<h1>Products</h1>
-
-			<p>Name: {products.name}</p>
+			{products && displayProductsInfo()}
+			{/* <p>Name: {products.name}</p>
 			<img src={productImage} alt="product" />
 			<p>Price: £{products.price}</p>
-			<p>Description: {products.description}</p>
+			<p>Description: {products.description}</p> */}
 		</div>
 	);
 };
