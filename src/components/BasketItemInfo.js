be2635b8986392cf0543beece7baa67cf9703059
Chris Fraser <chrisssfraser@gmail.com>
2020-12-09 18:36:06 +0000

    made basket an Object instead of Array

--- src/components/BasketItemInfo.js
+++ src/components/BasketItemInfo.js
@@ -2,19 +2,50 @@ import React from 'react';
 import './BasketItemInfo.css';
 
 const BasketItemInfo = ({ item, basketItems, setBasketItems }) => {
-	const handleRemoveItem = () => {
-		const basket = basketItems.concat();
-		const index = basket.indexOf(item);
-		basket.splice(index, 1);
-		setBasketItems(basket);
+
+	const handleRemoveAllItem = () => {
+	// 	const basket = basketItems.concat();
+	// 	const index = basket.indexOf(item);
+	// 	basket.splice(index, 1);
+	// 	setBasketItems(basket);
+
+	const basket = {...basketItems}
+	delete basket[item.product.image]
+	setBasketItems(basket)
 	};
+
+	const handleAddOneItem = () => {
+		const basket = {...basketItems}
+		basket[item.product.image]["amount"] += 1 
+		setBasketItems(basket)
+	}
+
+	const handleRemoveOneItem = () => {
+		const basket = {...basketItems}
+		if (basket[item.product.image]["amount"] === 1) {
+			delete basket[item.product.image]
+		} else {
+			basket[item.product.image]["amount"] -= 1
+		}
+		setBasketItems(basket)
+	}
+
+	const showNumberOfItem = () => {
+		let itemAmount = ""
+		if ( item.amount > 1) {
+			itemAmount = "x " + item.amount + " = £" + (item.amount * item.product.price).toFixed(2)
+		}
+		return itemAmount
+	}
 
 	return (
 		<div>
-			<p>{item.name}</p>
-			<p>£{item.price}</p>
-			<img src={item.imageSrc} alt={item.name} />
-			<button onClick={handleRemoveItem}>Remove</button>
+			<p>{item.product.name}</p>
+			<p>£{item.product.price} {showNumberOfItem()}</p>
+			<img src={item.product.imageSrc} alt={item.product.name} />
+			<button onClick={handleAddOneItem}>Add one</button>
+			<button onClick={handleRemoveOneItem}>Remove One</button>
+			<button onClick={handleRemoveAllItem}>Remove all</button>
 		</div>
 	);
 };
--- src/screens/Basket.js
+++ src/screens/Basket.js
@@ -3,19 +3,34 @@ import './Basket.css';
 import BasketItemInfo from '../components/BasketItemInfo.js';
 
 const Basket = ({ basketItems, setBasketItems }) => {
+
 	const displayBasketItems = () => {
-		const basket = basketItems.map((item, index) => {
-			return <BasketItemInfo item={item} basketItems={basketItems} setBasketItems={setBasketItems} key={index} />;
+	// 	const basket = basketItems.map((item, index) => {
+	// 		return <BasketItemInfo item={item} basketItems={basketItems} setBasketItems={setBasketItems} key={index} />;
+	// 	});
+	// 	return basket;
+
+		const basket = Object.keys(basketItems).map((key, index) => {
+			return <BasketItemInfo item={basketItems[key]} basketItems={basketItems} setBasketItems={setBasketItems} key={index} />;
 		});
 		return basket;
 	};
+
+
 	const basketTotal = () => {
+		// let total = 0;
+		// basketItems.forEach((item) => {
+		// 	total += item.price;
+		// });
+		// return total;
+
 		let total = 0;
-		basketItems.forEach((item) => {
-			total += item.price;
-		});
-		return total;
+		for (const [key, value] of Object.entries(basketItems)) {
+			total += ( value.product.price * value.amount )
+		}
+		return total.toFixed(2)
 	};
+
 	return (
 		<div>
 			<p>Basket</p>
