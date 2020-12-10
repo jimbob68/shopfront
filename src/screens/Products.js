a1071bdd7545f7022a8298ae3403773c4fac7bc6
James Fraser <jimbob4t@virginmedia.com>
2020-12-10 13:37:28 +0000

    Added filter by type dropdown

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -7,19 +7,29 @@ import db from '../firebaseConfig.js';
 
 const Products = ({ setBasketItems, basketItems }) => {
 	const [ products, setProducts ] = useState([]);
+	const [ productTypes, setProductTypes ] = useState([]);
+	const [ productsToDisplay, setProductsToDisplay ] = useState([]);
 
 	useEffect(() => {
 		retrieveProducts();
 	}, []);
 
+	useEffect(
+		() => {
+			getProductTypes();
+		},
+		[ products ]
+	);
+
 	const retrieveProducts = async () => {
 		const productsRef = db.collection('products');
 		const productsData = await productsRef.get();
 		setProducts(productsData.docs.map((doc) => doc.data()));
+		setProductsToDisplay(productsData.docs.map((doc) => doc.data()));
 	};
 
 	const displayProductsInfo = () => {
-		return products.map((product) => (
+		return productsToDisplay.map((product) => (
 			<ProductInfo
 				setBasketItems={setBasketItems}
 				basketItems={basketItems}
@@ -29,9 +39,43 @@ const Products = ({ setBasketItems, basketItems }) => {
 		));
 	};
 
+	const getProductTypes = () => {
+		const retrievedProductTypes = [];
+		const sortedProducts = products.map((product) => product.type).sort();
+		sortedProducts.forEach((product, index, array) => {
+			if (array[index] !== array[index + 1]) {
+				retrievedProductTypes.push(product);
+			}
+		});
+		setProductTypes(retrievedProductTypes);
+	};
+
+	const filterProductsByType = (type) => {
+		if (type === 'All Products') {
+			setProductsToDisplay(products);
+		} else {
+			const filteredProducts = products.filter((product) => product.type === type);
+			setProductsToDisplay(filteredProducts);
+		}
+	};
+
+	const displayTypesSelectBox = () => {
+		const selectOptions = productTypes.map((productType) => <option value={productType}>{productType}</option>);
+		return (
+			<select onChange={(event) => filterProductsByType(event.target.value)}>
+				<option selected disabled>
+					Filter by Category
+				</option>
+				<option value="All Products">All Products</option>
+				{selectOptions}
+			</select>
+		);
+	};
+
 	return (
 		<div>
 			<h1>Products</h1>
+			{displayTypesSelectBox()}
 			{/* {products && displayProductsInfo()} */}
 			{displayProductsInfo()}
 		</div>
